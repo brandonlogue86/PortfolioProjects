532abb9253aaf419944b296d3a9da422fe64cfed
Brandon Logue <logue.brandon86@gmail.com>
2025-02-04 16:12:17 -0500

    Pizza_Sales_SQL.sql

--- Pizza_Sales_SQL.sql
+++ Pizza_Sales_SQL.sql
@@ -21,9 +21,9 @@ FROM pizza_sales;
 
 -- Average Pizzas Per Order --
 SELECT CAST(
-		CAST(SUM(quantity) AS DECIMAL (10, 2))/
-		CAST(COUNT(DISTINCT order_id) AS DECIMAL (10, 2))
-		AS DECIMAL (10,2)) as avg_pizzas_per_order
+	CAST(SUM(quantity) AS DECIMAL (10, 2))/
+	CAST(COUNT(DISTINCT order_id) AS DECIMAL (10, 2))
+	AS DECIMAL (10,2)) as avg_pizzas_per_order
 FROM pizza_sales;
 
 -- Hourly Trend for Total Orders --
